9e6a63afb4a3d6a8ff50a77c173434fcd102a9b9
Erik Robstad <Erik.Robstad@nordicsemi.no>
2020-09-15 08:38:23 +0200

    Ready for lab 2

--- ex2/array/array.c
+++ ex2/array/array.c
@@ -5,67 +5,79 @@
 
 #include "array.h"
 
-
 // Construction / Destruction
 
-Array array_new(long capacity){
+Array array_new(long capacity)
+{
     assert(capacity > 0);
-    return (Array){malloc(sizeof(long)*capacity), 0, 0, capacity};
+    return (Array){malloc(sizeof(long) * capacity), 0, 0, capacity};
 }
 
-void array_destroy(Array a){
+void array_destroy(Array a)
+{
     free(a.data);
 }
 
-
 // Primitives
 
-long array_empty(Array a){
+long array_empty(Array a)
+{
     return a.back <= a.front;
 }
 
-long array_front(Array a){
+long array_front(Array a)
+{
     return a.data[a.front];
 }
 
-long array_back(Array a){
+long array_back(Array a)
+{
     return a.data[a.back - 1];
 }
 
-void array_popFront(Array* a){
+void array_popFront(Array *a)
+{
     a->front++;
 }
 
-void array_popBack(Array* a){
+void array_popBack(Array *a)
+{
     a->back--;
 }
 
-Array array_save(Array a){
+Array array_save(Array a)
+{
     return (Array){a.data, a.front, a.back, a.capacity};
 }
 
-
 // Iteration
 
-void array_foreach(Array a, void fn(long)){
-    for(Array b = array_save(a); !array_empty(b); array_popFront(&b)){
+void array_foreach(Array a, void fn(long))
+{
+    for (Array b = array_save(a); !array_empty(b); array_popFront(&b))
+    {
         fn(array_front(b));
     }
 }
 
-void array_foreachReverse(Array a, void fn(long)){
-    for(Array b = array_save(a); !array_empty(b); array_popBack(&b)){
+void array_foreachReverse(Array a, void fn(long))
+{
+    for (Array b = array_save(a); !array_empty(b); array_popBack(&b))
+    {
         fn(array_back(b));
     }
 }
 
-static void _array_printSingleLongHelper(long i){
+static void _array_printSingleLongHelper(long i)
+{
     printf(", %ld", i);
 }
 
-void array_print(Array a){
+void array_print(Array a)
+{
     printf("Array:{");
-    if(!array_empty(a)){
+    if (!array_empty(a))
+    {
         printf("%ld", array_front(a));
         array_popFront(&a);
     }
@@ -73,22 +85,31 @@ void array_print(Array a){
     printf("}\n");
 }
 
-
 // Capacity
 
-long array_length(Array a){
+long array_length(Array a)
+{
     return a.back - a.front;
 }
 
-void array_reserve(Array* a, long capacity){
-// TODO: your code here
+void array_reserve(Array *a, long capacity)
+{
+    long *newData = malloc(sizeof(long) * (capacity));
+    memcpy(newData, a->data, (sizeof(long) * (capacity / 2)));
+    free(a->data);
+    a->data = newData;
+    a->capacity = capacity;
 }
-
 
 // Modifiers
 
-void array_insertBack(Array* a, long stuff){
-// TODO: your code here
+void array_insertBack(Array *a, long stuff)
+{
+    // if (a->back >= a->capacity)
+    // {
+    //     array_reserve(a, ((a->capacity) * 2));
+    // }
+
+    a->data[a->back] = stuff;
+    a->back++;
 }
-
-
---
+++ ex2/testing.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "array.h"
+
+long xy_size = 1000 * 1000 * 1000; // 8 GB (sizeof(long) = 8 bytes)
+long x_dim = 100;
+long y_dim = xy_size / x_dim;
+
+long **matrix = malloc(y_dim * sizeof(long *));
+
+for (long y = 0; y < y_dim; y++)
+{
+    matrix[y] = malloc(x_dim * sizeof(long));
+}
+
+printf("Allocation complete (press any key to continue...)\n");
+getchar();
